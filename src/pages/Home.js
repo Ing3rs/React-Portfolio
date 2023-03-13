ffbc6f940ce4d0e161d1c81525b4239f3e941a9a
Claire Ingram <mail.claireingram+skillsfl@gmail.com>
2023-03-13 23:10:33 +0000

    added profile icon but needs centering

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import Hero from '../components/Hero';
 import Container from '../components/Container';
+import Row from '../components/Row';
+import Col from '../components/Col';
 
 
 // should be a welcome landing page that contains: 
@@ -15,11 +17,22 @@ function Home() {
 
         <div>
             <Hero>
-                    <h1>Hi there, I'm Claire</h1>
-                    <h2>I'm a Freelance Web Developer looking for their next challenge! </h2>
-                    <a className="btn contact-button" href="/contact" role="button">Work with me</a>
-                    <a className="btn projects-button" href="/projects" role="button">Latest projects</a>
-                    <a className="btn cv-button" href="/projects" role="button">My CV</a>
+                <Row>
+                    <Col size="lg-8 md-8 sm-12" gutter="4">
+                        <div>
+                            <h1>Hi there, I'm Claire</h1>
+                            <h2>I'm a Freelance Web Developer looking for their next challenge! </h2>
+                            <a className="btn contact-button" href="/contact" role="button">Work with me</a>
+                            <a className="btn projects-button" href="/projects" role="button">Latest projects</a>
+                            <a className="btn cv-button" href="/projects" role="button">My CV</a>
+                        </div>
+                    </Col>
+                    <Col size="lg-4 md-4 sm-12" gutter="4">
+                        <div>
+                        <img className="logo img-fluid" src="https://github.com/Ing3rs/challenge-3-boostrap-portfolio/raw/main/assets/images/claire-ingram-icon-bear-no-border.png" alt="profile-icon" />
+                        </div>
+                    </Col>
+                </Row>
             </Hero>
         </div>
     );
